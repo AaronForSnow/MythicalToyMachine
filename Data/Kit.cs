341e8e3b83afd814f324ed7d27e75e04cac44002
aaron.allen <aaron.allen@ENGR5L8P5S3>
2023-11-14 17:00:56 -0700

    It looks like it is working

--- Data/Kit.cs
+++ Data/Kit.cs
@@ -13,9 +13,17 @@ public partial class Kit
 
     public bool? Shoulddisplay { get; set; }
 
+    public int? CreatorId { get; set; }
+
     public string? ThumbnailPath { get; set; }
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual Customer? Creator { get; set; }
 
     public virtual Creature? Creature { get; set; }
 
     public virtual ICollection<KitAccessory> KitAccessories { get; set; } = new List<KitAccessory>();
+
+    public virtual ICollection<RequestItem> RequestItems { get; set; } = new List<RequestItem>();
 }
---
+++ Data/RequestItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MythicalToyMachine.Data;
+
+public partial class RequestItem
+{
+    public int Id { get; set; }
+
+    public int RequestId { get; set; }
+
+    public int KitId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal? RequestPrice { get; set; }
+
+    public virtual Kit Kit { get; set; } = null!;
+
+    public virtual Request Request { get; set; } = null!;
+}
