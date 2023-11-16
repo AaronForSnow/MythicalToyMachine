50e492a2c2334394f51943700076123105c98d59
Rachel Allen <rachel.allen1@students.snow.edu>
2023-11-16 10:19:22 -0700

    example of a mock and example of testing making a default class

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using Bunit.TestDoubles;
+using Moq;
+using MythicalToyMachine.Data;
+
 namespace UnitTests
 {
     public class UnitTest1
@@ -5,7 +9,28 @@ namespace UnitTests
         [Fact]
         public void Test1()
         {
+            var defaultRoles = new List<string> { "customer", "admin" };
+            var roleServiceMock = new Mock<IUserRoleService>();
+            roleServiceMock.Setup(m => m.IsAuthenticated).Returns(true);
+            roleServiceMock.Setup(m => m.Roles).Returns(defaultRoles);
 
+        }
+    }
+
+    public class UnitTestAuthenticationProvider : IUserRoleService
+    {
+        public bool IsAuthenticated => true;
+
+        public IEnumerable<string> Roles { get; }= new List<string> { "customer", "admin" };
+
+        public Task LookUpUser(string email, string name, string surname)
+        {
+            return Task.CompletedTask;
+        }
+
+        public void ResetUser()
+        {
+            
         }
     }
 }
