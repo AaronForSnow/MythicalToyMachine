89ed2eab2e152ba14f4181294d854d0c7711bfbf
rachel.allen1 <rachel.allen1@ENGR3HCP5S3>
2023-11-27 17:31:11 -0700

    Started to do the logic for actual purchase of a cart item

---
+++ Website/Data/DTOs/RequestItemDto.cs
@@ -0,0 +1,14 @@
+﻿namespace MythicalToyMachine.Data.DTOs;
+
+    public partial class RequestItemDto
+    {
+        public int Id { get; set; }
+
+        public int RequestId { get; set; }
+
+        public int KitId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal? RequestPrice { get; set; }
+    }
