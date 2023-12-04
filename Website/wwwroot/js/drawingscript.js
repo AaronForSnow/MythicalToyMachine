7de51a5ad839b28f94768cf2fcad54dd8173bb31
Dorian Cottle <thenewdorian21@gmail.com>
2023-12-03 21:48:40 -0700

    images stay when you drop one

--- Website/wwwroot/js/drawingscript.js
+++ Website/wwwroot/js/drawingscript.js
@@ -43,13 +43,102 @@ img.onload = function () {
         }
     });
 
-    function drawLine(x1, y1, x2, y2) {
-        ctx.beginPath();
-        ctx.strokeStyle = "red";
-        ctx.lineWidth = 10;
-        ctx.moveTo(x1, y1);
-        ctx.lineTo(x2, y2);
-        ctx.stroke();
-        ctx.closePath();
+function drawLine(x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.lineWidth = 10;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+
+
+///////////////////////////////////////////////
+//dragondrop
+
+
+let draggedElement = null;
+
+const dragContainer = document.getElementById('dragContainer');
+console.log('drag container', dragContainer);
+const dragImgs = document.querySelectorAll('.accessory-img');
+console.log('draggables', dragImgs);
+
+
+
+const handleDragStart = function (e) {
+    e.dataTransfer.setData('text/plain', e.target.id);
+    console.log('targetid', e.target);
+    draggedElement = e.target;
+    console.log('draggedelement set', draggedElement);
+};
+
+const handleDragOver = function (e) {
+    e.preventDefault();
+    dragContainer.classList.add('dragOver');
+};
+
+const handleDrop = function (e) {
+    e.preventDefault();
+    if (draggedElement) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+
+        // Calculate the offset dynamically based on the pig's size
+        const offsetX = draggedElement.clientWidth / 2;
+        const offsetY = draggedElement.clientHeight * 1.5;
+
+        // Dropped in specified spot
+        draggedElement.style.position = 'absolute';
+        draggedElement.style.left = `${mouseX}px`;
+        draggedElement.style.top = `${mouseY}px`;
+
+        // Explicitly set width and height to maintain consistency
+        draggedElement.style.width = `${draggedElement.clientWidth}px`;
+        draggedElement.style.height = `${draggedElement.clientHeight}px`;
+
+        let copyOfDraggedElement = draggedElement;
+        draggedElement = null;
+        const accessoryColumn = document.getElementById('accessory-column');
+        accessoryColumn.innerHTML = `
+         <Div Class="accessory-img img-fluid" id="unicornhorn">
+                <img class="img-fluid" src="/Images/Accessories/unicornhorn.png">
+            </Div>
+            <Div Class="accessory-img img-fluid" id="antlers">
+                <img class="img-fluid" src="/Images/Accessories/antlers.png">
+            </Div>
+            <Div Class="accessory-img img-fluid" id="wing">
+                <img class="img-fluid" src="/Images/Accessories/wing.png">
+            </Div>
+            <Div Class="accessory-img img-fluid" id="wing-evil">
+                <img class="img-fluid" src="/Images/Accessories/wing-evil.png">
+            </Div>
+            <Div Class="accessory-img img-fluid" id="js">
+                <img class="img-fluid" src="/Images/Accessories/js.png">
+            </Div>
+        `;
+        accessoryColumn.appendChild(copyOfDraggedElement);
+
+        dragContainer.classList.remove('dragOver');
     }
+};
+
+const handleDragLeave = function () {
+    dragContainer.classList.remove('dragOver');
+};
+
+
+
+dragImgs.forEach(function (dragImg) {
+    dragImg.addEventListener('dragstart', handleDragStart);
+});
+
+dragContainer.addEventListener('dragover', handleDragOver);
+dragContainer.addEventListener('dragleave', handleDragLeave);
+dragContainer.addEventListener('drop', handleDrop);
+
+
+
 
