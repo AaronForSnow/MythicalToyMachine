88f255a5ebb18b1ceaf0041ae0d485873a5f8781
Carlos Blanco <blancocastrocarlosfelipe@gmail.com>
2023-12-04 01:16:45 -0700

    Improved the creation page

---
+++ UnitTests/BlazorIntegrationTestContext.cs
@@ -0,0 +1,43 @@
+﻿using Bunit;
+using FluentAssertions.Common;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MythicalToyMachine.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.PostgreSql;
+
+namespace UnitTests
+{
+    public class BlazorIntegrationTestContext : TestContext, IAsyncLifetime
+    {
+        private readonly PostgreSqlContainer _dbContainer;
+
+        public BlazorIntegrationTestContext()
+        {
+            _dbContainer = new PostgreSqlBuilder()
+                .WithImage("postgres")
+                .WithPassword("Strong_password_123!")
+                .Build();
+
+            Services.AddDbContextFactory<PostgresContext>(options => options.UseNpgsql(_dbContainer.GetConnectionString()));
+            Services.AddScoped<WeatherForecastService>();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _dbContainer.StartAsync();
+
+            var dbContext = Services.GetRequiredService<PostgresContext>();
+            await dbContext.Database.MigrateAsync();
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            await _dbContainer.StopAsync();
+        }
+    }
+}
---
+++ UnitTests/ToyIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Bunit;
+using FluentAssertions;
+using MythicalToyMachine.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class ToyIntegrationTests : BlazorIntegrationTestContext
+    {
+        [Fact]
+        public async Task CanMakeComponent()
+        {
+            var cut = RenderComponent<Cart>();
+
+            cut.Find("a").InnerHtml.Should().Be("You are not logged in. Log in to see your cart.");
+
+            cut.WaitForElements("tbody tr").Should().HaveCount(5);
+        }
+
+        [Fact]
+        public async Task RenderingTwiceMakes10Forecasts()
+        {
+            var cut = RenderComponent<Cart>();
+
+            cut.Find("p em").InnerHtml.Should().Be("Loading...");
+
+            cut.WaitForElements("tbody tr").Should().HaveCount(5);
+
+            var c2 = RenderComponent<Cart>();
+            c2.WaitForElements("tbody tr").Should().HaveCount(10);
+
+            var c3 = RenderComponent<Cart>();
+            c3.WaitForElements("tbody tr").Should().HaveCount(15);
+        }
+    }
+}
