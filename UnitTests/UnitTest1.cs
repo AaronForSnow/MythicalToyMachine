b84d73f5e0cd82f613098aa72c2e8857de49ab4f
Dorian Cottle <thenewdorian21@gmail.com>
2023-12-05 18:27:45 -0700

    fixed an error i made

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -7,85 +7,88 @@ using MythicalToyMachine.Data;
 using MythicalToyMachine.Pages;
 using MythicalToyMachine.Services;
 
-namespace UnitTests
+namespace UnitTests;
+
+public class UnitTest1 : TestContext
 {
-    public class UnitTest1 : TestContext
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
-            var defaultRoles = new List<string> { "customer", "admin" };
-            var roleServiceMock = new Mock<IUserRoleService>();
-            roleServiceMock.Setup(m => m.IsAuthenticated).Returns(true);
-            roleServiceMock.Setup(m => m.Roles).Returns(defaultRoles);
+        var defaultRoles = new List<string> { "customer", "admin" };
+        var roleServiceMock = new Mock<IUserRoleService>();
+        roleServiceMock.Setup(m => m.IsAuthenticated).Returns(true);
+        roleServiceMock.Setup(m => m.Roles).Returns(defaultRoles);
 
-        }
+    }
 
-    //    [Fact]
-    //    public void ClickingAddToCartButtonPutsTheItemInTheShoppingCart()
+    //[Fact]
+    //public void ClickingAddToCartButtonPutsTheItemInTheShoppingCart()
+    //{
+    //    //Arrange
+    //    var cartService = new ShoppingCartService();
+    //    Services.AddSingleton<IUserRoleService, UnitTestAuthenticationProvider>();
+    //    //Services.AddSingleton<ShoppingCartService>(cartService);
+    //    //Services.AddDbContextFactory<PostgresContext>();
+    //    //Services.AddHttpContextAccessor();
+    //    //Services.AddScoped<HttpContextAccessor>();
+    //    //Services.AddAuthentication().AddGoogle(options =>
+    //    //{
+    //    //    //var clientid = builder.Configuration["Google:ClientId"];
+    //    //    options.ClientId = builConfiguration["Google:ClientId"];
+    //    //    options.ClientSecret = builder.Configuration["Google:ClientSecret"];
+    //    //    //options.ClaimActions.MapJsonkey("urn:google:profile", "link");
+    //    //    //options.ClaimActions.MapJsonkey("urn:google:image", "picture");
+    //    //});
+    //    var mockService = new Mock<IDataService>();
+    //    mockService.Setup(m => m.GetKitsAsync()).ReturnsAsync(new[]
     //    {
-    //        //Arrange
-    //        var cartService = new ShoppingCartService();
-    //        Services.AddSingleton<IUserRoleService, UnitTestAuthenticationProvider>();
-    //        //Services.AddSingleton<ShoppingCartService>(cartService);
-    //        //Services.AddDbContextFactory<PostgresContext>();
-    //        //Services.AddHttpContextAccessor();
-    //        //Services.AddScoped<HttpContextAccessor>();
-    //        //Services.AddAuthentication().AddGoogle(options =>
-    //        //{
-    //        //    //var clientid = builder.Configuration["Google:ClientId"];
-    //        //    options.ClientId = builConfiguration["Google:ClientId"];
-    //        //    options.ClientSecret = builder.Configuration["Google:ClientSecret"];
-    //        //    //options.ClaimActions.MapJsonkey("urn:google:profile", "link");
-    //        //    //options.ClaimActions.MapJsonkey("urn:google:image", "picture");
-    //        //});
-    //        var mockService = new Mock<IDataService>();
-    //        mockService.Setup(m => m.GetKitsAsync()).ReturnsAsync(new[]
+    //        new Kit
     //        {
-    //            new Kit
-    //            {
-    //                Id = 1,
-    //                Kitname = "BogusKit1"
-    //            }
-    //        });
-    //        Services.AddTransient<IDataService>(_ => mockService.Object);
-    //        var cut = RenderComponent<Shop>();
+    //            Id = 1,
+    //            Kitname = "BogusKit1"
+    //        }
+    //    });
+    //    Services.AddTransient<IDataService>(_ => mockService.Object);
+    //    var cut = RenderComponent<Shop>();
 
-    //        //act
-    //        var button = cut.WaitForElement(".cartButton");
-    //        button.Click();
+    //    //act
+    //    var button = cut.WaitForElement(".cartButton");
+    //    button.Click();
 
-    //        //assert
-    //        Assert.Equal(1, cartService.AllKitsThatAreInTheCart.Count);
-    //        Assert.Equal("BogusKit1", cartService.AllKitsThatAreInTheCart[0].Kitname);
-    //    }
+    //    //assert
+    //    Assert.Equal(1, cartService.AllKitsThatAreInTheCart.Count);
+    //    Assert.Equal("BogusKit1", cartService.AllKitsThatAreInTheCart[0].Kitname);
     //}
 
-    public class UnitTestAuthenticationProvider : IUserRoleService
+}
+
+public class UnitTestAuthenticationProvider : IUserRoleService
+{
+    public bool IsAuthenticated => true;
+
+    public IEnumerable<string> Roles { get; } = new List<string> { "customer", "admin" };
+
+    public Task<Customer> GetUser(string email)
     {
-        public bool IsAuthenticated => true;
+        throw new NotImplementedException();
+    }
 
-        public IEnumerable<string> Roles { get; } = new List<string> { "customer", "admin" };
+    public Task<int> LookUpUserAsync(string email, string name, string surname)
+    {
+        return (Task<int>)Task.CompletedTask;
+    }
 
-        public Task<Customer> GetUser(string email)
-        {
-            throw new NotImplementedException();
-        }
+    public void ResetUser()
+    {
 
-        public Task<int> LookUpUserAsync(string email, string name, string surname)
-        {
-            return (Task<int>)Task.CompletedTask;
-        }
+    }
+    public PostgresContext GetPostgresContext()
+    {
+        throw new NotImplementedException();
+    }
 
-        public void ResetUser()
-        {
-
-        }
-        public PostgresContext GetPostgresContext()
-        {
-            throw new NotImplementedException();
-        }
-
-
+    public Task<int> LookUpUser(string email, string name, string surname)
+    {
+        throw new NotImplementedException();
     }
 }
