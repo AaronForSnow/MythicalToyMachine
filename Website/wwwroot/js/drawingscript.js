8ec5cdd48414d9e62ae13456c7a2ba93091d091a
Dorian Cottle <thenewdorian21@gmail.com>
2023-12-06 17:07:21 -0700

    made the colors touch compatible

--- Website/wwwroot/js/drawingscript.js
+++ Website/wwwroot/js/drawingscript.js
@@ -1,49 +1,61 @@
-    var x = 0;
-    var y = 0;
+var x = 0;
+var y = 0;
 var isDrawing = false;
-    
-    var cnv = document.getElementById("dinoCanvas");
+
+var cnv = document.getElementById("dinoCanvas");
 var ctx = cnv.getContext("2d");
 
 var img = new Image();
 
-// Set the source of the image
+
 img.src = 'Images/Accessories/dino.png';
 
-// When the image is loaded, draw it on the canvas
+
 img.onload = function () {
-    // Draw the image at position (0, 0) on the canvas
     ctx.drawImage(img, 0, 0, 500, 300); //last 2 args define img dimensions
 }
 
+cnv.addEventListener("mousedown", startDrawing);
+cnv.addEventListener("mousemove", draw);
+cnv.addEventListener("mouseup", stopDrawing);
 
-    cnv.addEventListener("mousedown", (e) => {
-        console.log("mouse has been clicked");
-        x = e.offsetX;
-        y = e.offsetY;
-        isDrawing = true
-    });
-
-    cnv.addEventListener("mousemove", (e) => {
-        if (isDrawing === true) {
-            drawLine(x/2, y/2, e.offsetX/2, e.offsetY/2);
-            x = e.offsetX ;
-            y = e.offsetY ;
-        }
-    });
-
-    cnv.addEventListener("mouseup", (e) => {
-        console.log("mouse has been unclicked");
-        if (isDrawing === true) {
-
-            drawLine(x, y, e.offsetX, e.offsetY);
-            x = 0;
-            y = 0;
-            isDrawing = false;
-        }
-    });
+cnv.addEventListener("touchstart", startDrawing);
+cnv.addEventListener("touchmove", draw);
+cnv.addEventListener("touchend", stopDrawing);
 
 let color = 'yellow';
+
+function startDrawing(e) {
+    e.preventDefault();
+    console.log("Drawing started");
+    x = (e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / 2;
+    y = (e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / 2;
+    isDrawing = true;
+}
+
+function draw(e) {
+    e.preventDefault();
+    if (isDrawing === true) {
+        const currentX = (e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / 2;
+        const currentY = (e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / 2;
+        drawLine(x, y, currentX, currentY);
+        x = currentX;
+        y = currentY;
+    }
+}
+
+function stopDrawing(e) {
+    e.preventDefault();
+    console.log("Drawing stopped");
+    if (isDrawing === true) {
+        const endX = (e.type.startsWith('touch') ? e.changedTouches[0].clientX : e.offsetX) / 2;
+        const endY = (e.type.startsWith('touch') ? e.changedTouches[0].clientY : e.offsetY) / 2;
+        drawLine(x, y, endX, endY);
+        x = 0;
+        y = 0;
+        isDrawing = false;
+    }
+}
 
 function drawLine(x1, y1, x2, y2) {
     ctx.beginPath();
@@ -55,8 +67,10 @@ function drawLine(x1, y1, x2, y2) {
     ctx.closePath();
 }
 
-const selectedColor = document.querySelectorAll('color-selection');
+const selectedColor = document.querySelectorAll('.color-selection');
 console.log('colorselects', selectedColor);
+
+
 
 
 
@@ -64,20 +78,17 @@ console.log('colorselects', selectedColor);
 //dragondrop
 
 
+
 let draggedElement = null;
 
 const dragContainer = document.getElementById('dragContainer');
-console.log('drag container', dragContainer);
 const dragImgs = document.querySelectorAll('.accessory-img');
-console.log('draggables', dragImgs);
 
 
 
 const handleDragStart = function (e) {
     e.dataTransfer.setData('text/plain', e.target.id);
-    console.log('targetid', e.target);
     draggedElement = e.target;
-    console.log('draggedelement set', draggedElement);
 };
 
 const handleDragOver = function (e) {
@@ -94,12 +105,11 @@ const handleDrop = function (e) {
         const offsetX = draggedElement.clientWidth / 2;
         const offsetY = (draggedElement.clientHeight * 1.5) - 180;
 
-        const copyOfDraggedElement = draggedElement.cloneNode(true); // Create a deep copy of the dragged element
+        const copyOfDraggedElement = draggedElement.cloneNode(true); 
         copyOfDraggedElement.style.position = 'absolute';
         copyOfDraggedElement.style.left = `${mouseX - offsetX}px`;
         copyOfDraggedElement.style.top = `${mouseY - offsetY}px`;
 
-        //Explicitly set width and height to maintain consistency
         copyOfDraggedElement.style.width = `${draggedElement.clientWidth}px`;
         copyOfDraggedElement.style.height = `${draggedElement.clientHeight}px`;
 
@@ -124,7 +134,3 @@ dragImgs.forEach(function (dragImg) {
 dragContainer.addEventListener('dragover', handleDragOver);
 dragContainer.addEventListener('dragleave', handleDragLeave);
 dragContainer.addEventListener('drop', handleDrop);
-
-
-
-
