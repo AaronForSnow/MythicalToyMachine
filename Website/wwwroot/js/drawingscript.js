606e326ad98b1c86c75b9932467dcc753f339f35
Dorian C <thenewdorian21@gmail.com>
2023-12-07 18:49:30 -0700

    offset complete!

--- Website/wwwroot/js/drawingscript.js
+++ Website/wwwroot/js/drawingscript.js
@@ -2,8 +2,17 @@ var x = 0;
 var y = 0;
 var isDrawing = false;
 
+
+let draggedElement = null;
+
+const dragContainer = document.getElementById('dragContainer');
+const dragImgs = document.querySelectorAll('.accessory-img');
+
 var cnv = document.getElementById("dinoCanvas");
 var ctx = cnv.getContext("2d");
+
+cnv.width = dragContainer.clientWidth;
+cnv.height = dragContainer.clientHeight;
 
 var img = new Image();
 
@@ -11,8 +20,18 @@ var img = new Image();
 img.src = 'Images/Accessories/dino.png';
 
 
+//window.addEventListener("resize", resizeCanvas); //THIS CONTROLS THE CHANGING OF THE CANVAS WHEN YOU DO DIFFERENT SIZES
+
+function resizeCanvas(e) {
+    console.log("changing canvas. Old width:", cnv.width);
+    cnv.width = dragContainer.clientWidth;
+    cnv.height = dragContainer.clientHeight;
+    console.log("new width:", cnv.width);
+}
+
+
 img.onload = function () {
-    ctx.drawImage(img, 0, 0, 500, 300); //last 2 args define img dimensions
+    ctx.drawImage(img, 0, 0, cnv.width, cnv.height); //last 2 args define img dimensions
 }
 
 cnv.addEventListener("mousedown", startDrawing);
@@ -23,22 +42,24 @@ cnv.addEventListener("touchstart", startDrawing);
 cnv.addEventListener("touchmove", draw);
 cnv.addEventListener("touchend", stopDrawing);
 
-let color = 'yellow';
+let color = 'red';
 
 function startDrawing(e) {
     e.preventDefault();
     console.log("Drawing started");
-    x = (e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / 2;
-    y = (e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / 2;
+    x = e.offsetX; //(e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / cnv.width;
+    y = e.offsetY;//(e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / cnv.height;
     isDrawing = true;
 }
 
 function draw(e) {
     e.preventDefault();
     if (isDrawing === true) {
-        const currentX = (e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / 2;
-        const currentY = (e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / 2;
+        const currentX = e.offsetX;//(e.type.startsWith('touch') ? e.touches[0].clientX : e.offsetX) / cnv.width;
+        const currentY = e.offsetY;// (e.type.startsWith('touch') ? e.touches[0].clientY : e.offsetY) / cnv.height;
         drawLine(x, y, currentX, currentY);
+        console.log("currentx", currentX);
+        //console.log("currenty", currentY);
         x = currentX;
         y = currentY;
     }
@@ -48,8 +69,8 @@ function stopDrawing(e) {
     e.preventDefault();
     console.log("Drawing stopped");
     if (isDrawing === true) {
-        const endX = (e.type.startsWith('touch') ? e.changedTouches[0].clientX : e.offsetX) / 2;
-        const endY = (e.type.startsWith('touch') ? e.changedTouches[0].clientY : e.offsetY) / 2;
+        const endX = e.offsetX;//(e.type.startsWith('touch') ? e.changedTouches[0].clientX : e.offsetX) / cnv.width;
+        const endY = e.offsetY;//(e.type.startsWith('touch') ? e.changedTouches[0].clientY : e.offsetY) / cnv.height;
         drawLine(x, y, endX, endY);
         x = 0;
         y = 0;
@@ -94,10 +115,7 @@ selectedColor.forEach(function (colorDiv) {
 
 
 
-let draggedElement = null;
 
-const dragContainer = document.getElementById('dragContainer');
-const dragImgs = document.querySelectorAll('.accessory-img');
 
 
 
